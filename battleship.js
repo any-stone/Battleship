259671c5096b148768638875cf67be86409a7d4b
any-stone <kuxa.business@gmail.com>
2022-07-11 23:19:02 -0600

    first commit

---
+++ battleship.js
@@ -0,0 +1,30 @@
+//Ship locations
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+//Dealing with user's guess
+var guess;
+var hits = 0;
+var guesses = 0;
+
+//checking if the ship is sunk
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! Enter a number from 0-6:");
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid number!");
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        }
+    }
+}
