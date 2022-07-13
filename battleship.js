fb32eb94fbef79c7b2eca5b08ae48a101813ef53
any-stone <kuxa.business@gmail.com>
2022-07-12 22:48:46 -0600

    added alerts for hit and miss, added stats

--- battleship.js
+++ battleship.js
@@ -12,19 +12,32 @@ var isSunk = false;
 
 //checking the user's guess and whether the ship is sunk
 while (isSunk == false) {
-    guess = prompt("Ready, aim, fire! Enter a number from 0-6:");
-    if (guess < 0 || guess > 6) {
-        alert("Please enter a valid number!");
+  guess = prompt("Ready, aim, fire! Enter a number from 0-6:");
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a valid number!");
+  } else {
+    guesses = guesses + 1;
+
+    if (guess == location1 || guess == location2 || guess == location3) {
+      alert("HIT!");
+      hits = hits + 1;
+
+      if (hits == 3) {
+        isSunk = true;
+        alert("You sank my battleship!");
+      }
     } else {
-        guesses = guesses + 1;
-
-        if (guess == location1 || guess == location2 || guess == location3) {
-            hits = hits + 1;
-
-            if (hits == 3) {
-                isSunk = true;
-                alert("You sank my battleship!");
-            }
-        }
+      alert("MISS!");
     }
+  }
 }
+
+//tell user stats
+var stats =
+  "You took " +
+  guesses +
+  " guesses to sink the battleship, " +
+  "which means your shooting accuracy was " +
+  3 / guesses;
+
+alert(stats);
